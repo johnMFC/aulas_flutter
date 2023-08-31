574bca7d3620ba965aecda0ba956f55e7630f4c4
Johnatas michaell <johnatasmichael067@gmail.com>
2023-08-31 14:41:44 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aulas_perguntas2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aulas_perguntas2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './respostas.dart';
+
+void main() => runApp(const PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      { 
+        'texto' : ' Qual é sua cor favorita?',
+        'resposta' : [ 'Preto','Vermelho', 'Laranja','Roxo'],
+
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'resposta' : [ 'Cachorro','Coelho', 'Gato','Leão' ],
+      },
+      {
+        'texto' : 'Qual é seu instrutor favorito ?',
+        'resposta' : [ 'Maria','joão', 'John','vitor' ],
+      }
+       ];
+
+       List<String> resposta = perguntas[_perguntaSelecionada].cast()['resposta'];
+       List<Widget> widgets = resposta.map((t) => Resposta);
+       /*for( String  textoResp in resposta ){
+          widgets.add(Resposta(textoResp, _responder));
+       }*/
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Questao(perguntas[_perguntaSelecionada]['texto'] .toString()),
+            ...resposta,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
